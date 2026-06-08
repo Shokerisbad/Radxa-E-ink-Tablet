d9854a2e77d67160903ef083f70af254b4e2b5d5
Shokerisbad <mihalache.petre.cristi@gmail.com>
2026-06-08 19:06:00 +0300

    changed how the books are named

--- TabletClient/src/App.cpp
+++ TabletClient/src/App.cpp
@@ -695,12 +695,12 @@ static void build_library_list(SortMode mode) {
       
       lv_obj_t *lbl_title = lv_label_create(btn);
       lv_label_set_text(lbl_title, g_book_metadata[path_str].title.c_str());
-      lv_label_set_long_mode(lbl_title, LV_LABEL_LONG_CROP);
+      lv_label_set_long_mode(lbl_title, LV_LABEL_LONG_CLIP);
       lv_obj_set_width(lbl_title, 280);
 
       lv_obj_t *lbl_author = lv_label_create(btn);
       lv_label_set_text(lbl_author, g_book_metadata[path_str].author.c_str());
-      lv_label_set_long_mode(lbl_author, LV_LABEL_LONG_CROP);
+      lv_label_set_long_mode(lbl_author, LV_LABEL_LONG_CLIP);
       lv_obj_set_width(lbl_author, 280);
       lv_obj_set_style_text_color(lbl_author, lv_color_hex(0x666666), 0); // Gray text for author
 
